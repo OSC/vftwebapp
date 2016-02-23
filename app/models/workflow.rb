9c4d9fc700969130c31c5fa47064381826a8c871
Jeremy Nicklas <jnicklas@osc.edu>
2016-02-23 09:18:29 -0500

    dont delete staged_dir if bad job submission

--- app/models/workflow.rb
+++ app/models/workflow.rb
@@ -11,7 +11,23 @@ class Workflow < ActiveRecord::Base
     "oakley"
   end
 
+  # Good default job builder for single job script
   def build_jobs(staged_dir, job_list = [])
     job_list << OSC::Machete::Job.new(script: staged_dir.join(script_name), host: host)
+  end
+
+  # Override Machete Workflow submit so that it doesn't delete the staged_dir
+  # when job submission fails
+  def submit(template_view=self)
+    staged_dir = stage
+    render_mustache_files(staged_dir, template_view)
+    after_stage(staged_dir)
+    jobs = build_jobs(staged_dir)
+    if submit_jobs(jobs)
+      save_jobs(jobs, staged_dir)
+    else
+      # FileUtils.rm_rf staged_dir.to_s
+      false
+    end
   end
 end
