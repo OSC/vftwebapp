99c8206faeafe094eee068bc776329866cf84279
Eric Franz <efranz@osc.edu>
2016-02-24 11:00:58 -0500

    add partials

--- app/view_models/view_model.rb
+++ app/view_models/view_model.rb
@@ -2,19 +2,26 @@ class ViewModel < SimpleDelegator
   def to_partial_path
     "sessions/#{self.class.name.underscore}"
   end
+  
+  def workflow_stage
+    ""
+  end
 
   def self.for_session(session)
+    thermal = session.thermal
+    structural = thermal ? session.thermal.structural : nil
+
     if ! session.submitted?
       VftsolidFormView.new(session)
     elsif session.running? || session.failed?
       VftsolidStatusView.new(session)
     elsif session.passed? && ! thermal.submitted?
       ThermalFormView.new(session)
-    elsif thermal.running? || thermal.failed?
+    elsif thermal && (thermal.active? || thermal.failed?)
       ThermalStatusView.new(session)
-    elsif thermal.passed? && ! structural.submitted?
+    elsif thermal && (thermal.passed? && ! structural.submitted?)
       StructuralFormView.new(session)
-    elsif structural.running? || structural.failed?
+    elsif structural && (structural.active? || structural.failed?)
       StructuralStatusView.new(session)
     else
       ResultsView.new(session)
