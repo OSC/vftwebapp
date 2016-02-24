f211b434d7fd4ea36b71a83c34510de658122b05
Eric Franz <efranz@osc.edu>
2016-02-24 11:09:47 -0500

    color all active rows yellow

--- app/view_models/view_model.rb
+++ app/view_models/view_model.rb
@@ -3,12 +3,17 @@ class ViewModel < SimpleDelegator
     "sessions/#{self.class.name.underscore}"
   end
   
+  #FIXME: should the subclasses specify these?
   def workflow_stage
     return "1/3" if [VftsolidFormView, VftsolidStatusView].include? self.class
     return "2/3" if [ThermalFormView, ThermalStatusView].include? self.class
     return "3/3" if [StructuralFormView, StructuralStatusView].include? self.class
 
     "Results:"
+  end
+
+  def row_bg
+    self.class == ResultsView ? "" : "bg-warning"
   end
 
   def self.for_session(session)
