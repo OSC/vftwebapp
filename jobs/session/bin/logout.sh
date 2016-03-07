8bf9cf225d25420c65db5b44787cf90de597cd1e
Jeremy Nicklas <jnicklas@osc.edu>
2016-03-07 10:38:54 -0500

    confirm logout

--- jobs/session/bin/logout.sh
+++ jobs/session/bin/logout.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-SLEEP_PID=$(pgrep -s 0 sleep)
-kill -9 ${SLEEP_PID}
+if zenity --question --text="<span size=\"x-large\">Are you sure you want to log out?</span>" --title="Log out"; then
+  SLEEP_PID=$(pgrep -s 0 sleep)
+  kill -9 ${SLEEP_PID}
+fi
