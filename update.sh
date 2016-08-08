6f9d3fefd1d42edc7507fada3cd52b6e63e1b41a
Jeremy Nicklas <jnicklas@osc.edu>
2016-08-08 10:16:21 -0400

    add update script

---
+++ update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+
+module load ruby
+
+if [ $# -eq 0  ]; then
+  echo "Please supply a tag version"
+  echo "e.g.: $0 v0.0.2"
+  exit 1
+fi
+
+git fetch --tags
+git checkout ${2}
+if [ -d "wiki" ]; then
+  cd wiki
+  git pull
+  cd ..
+fi
+bin/bundle install --path=vendor/bundle
+bin/rake assets:clobber RAILS_ENV=production
+bin/rake assets:precompile RAILS_ENV=production
+bin/rake tmp:clear RAILS_ENV=production
