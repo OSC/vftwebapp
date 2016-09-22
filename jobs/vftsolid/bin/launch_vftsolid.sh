04e9c79fa5d831f1d4cac4323b0b348a599865e3
Jeremy Nicklas <jnicklas@osc.edu>
2016-09-22 09:51:53 -0400

    use wiag home dir for custom modules

--- jobs/vftsolid/bin/launch_vftsolid.sh
+++ jobs/vftsolid/bin/launch_vftsolid.sh
@@ -3,9 +3,9 @@
 FvwmCommand "Restart"
 
 . /etc/profile.d/lmod.sh
-module use /nfs/gpfs/PZS0645/local-ruby/share/modulefiles
+module use /users/PZS0645/wiag/local-ruby/share/modulefiles
 module load wine
-module use /nfs/gpfs/PZS0645/local-ruby/emc2/share/modulefiles
+module use /users/PZS0645/wiag/local-ruby/emc2/share/modulefiles
 module load vft/i686
 
 cd ${VFT_HOME}
