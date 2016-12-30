b7c02ef17c026739a8bdba116c85836656542f93
Jeremy Nicklas <jnicklas@osc.edu>
2016-12-30 11:51:09 -0500

    utilize ctsp binary in vftsolid

--- jobs/vftsolid/bin/launch_vftsolid.sh
+++ jobs/vftsolid/bin/launch_vftsolid.sh
@@ -6,7 +6,8 @@ FvwmCommand "Restart"
 module use /users/PZS0645/wiag/local-ruby/share/modulefiles
 module load wine
 module use /users/PZS0645/wiag/local-ruby/emc2/share/modulefiles
+module load ctsp    # necessary for writing out WARP3D cut
 module load vft/i686
 
 cd ${VFT_HOME}
-VFTHOME="Z:\\${PBS_O_WORKDIR}" MATDIR="${MATDIR}" wine Project2.exe
+VFTHOME="Z:\\${PBS_O_WORKDIR}" MATDIR="${MATDIR}" TEMP_CONVERT="$(command -v temp_convert.x)" wine Project2.exe
