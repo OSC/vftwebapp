1d5595398c31d2fa67f50db294e3d1c43e7bdd2d
Jeff Ohrstrom <johrstrom@osc.edu>
2021-04-01 13:21:18 -0400

    keep original ood_appkit.rb and append OODClusters to it

--- config/initializers/ood_appkit.rb
+++ config/initializers/ood_appkit.rb
@@ -1,5 +1,17 @@
-# config/initializers/ood_appkit.rb
-require "ood_core"
+# OodAppkit configuration
+
+class MyFilesRackApp < OodAppkit::FilesRackApp
+  def call(env)
+    Rack::Directory.new(
+      root,
+      Rack::File.new(root, {'Content-Disposition' => 'attachment'}, 'application/octet-stream')
+    ).call(env)
+  end
+end
+
+OodAppkit.configure do |config|
+  config.routes.files_rack_app = false
+end
 
 OODClusters = OodCore::Clusters.new(
   OodAppkit.clusters.select(&:job_allow?)
