355fac3f102d85ac354ac0e4df0635f664340936
Jeff Ohrstrom <johrstrom@osc.edu>
2021-04-01 12:04:26 -0400

    initial commit to get slurm working
    This works to some degree. It seems to be able to submit jobs and manage
    their lifecycle (i.e., it recognizes job states, queued, running & completed).
    Though the jobs themselves do not yet work.

--- config/initializers/ood_appkit.rb
+++ config/initializers/ood_appkit.rb
@@ -1,14 +1,6 @@
-# OodAppkit configuration
+# config/initializers/ood_appkit.rb
+require "ood_core"
 
-class MyFilesRackApp < OodAppkit::FilesRackApp
-  def call(env)
-    Rack::Directory.new(
-      root,
-      Rack::File.new(root, {'Content-Disposition' => 'attachment'}, 'application/octet-stream')
-    ).call(env)
-  end
-end
-
-OodAppkit.configure do |config|
-  config.routes.files_rack_app = false
-end
+OODClusters = OodCore::Clusters.new(
+  OodAppkit.clusters.select(&:job_allow?)
+)
