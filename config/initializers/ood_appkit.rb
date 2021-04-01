799388af41137b85e33895ccb8ded4286ab0d319
Eric Franz <efranz@osc.edu>
2021-04-01 14:13:09 -0400

    Update to slurm fixes (#98)
    updates that:
    * switch to SLURM vars & owens modules
    * fix conn file path
    * add back files rack app

--- config/initializers/ood_appkit.rb
+++ config/initializers/ood_appkit.rb
@@ -13,6 +13,19 @@ OodAppkit.configure do |config|
   config.routes.files_rack_app = false
 end
 
+class MyFilesRackApp < OodAppkit::FilesRackApp
+  def call(env)
+    Rack::Directory.new(
+      root,
+      Rack::File.new(root, {'Content-Disposition' => 'attachment'}, 'application/octet-stream')
+    ).call(env)
+  end
+end
+
+OodAppkit.configure do |config|
+  config.routes.files_rack_app = false
+end
+
 OODClusters = OodCore::Clusters.new(
   OodAppkit.clusters.select(&:job_allow?)
 )
