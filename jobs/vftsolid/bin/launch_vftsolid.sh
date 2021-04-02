2e231c2dcfcf16b27fc25b5d76956ab8a8a852c3
Eric Franz <efranz@osc.edu>
2021-04-01 20:29:04 -0400

    use ruby wine build for now

--- jobs/vftsolid/bin/launch_vftsolid.sh
+++ jobs/vftsolid/bin/launch_vftsolid.sh
@@ -4,7 +4,10 @@ FvwmCommand "Restart"
 
 . /etc/profile.d/lmod.sh
 
-module load wine/5.1
+
+# module load wine/5.1
+module use /users/PZS0645/wiag/local-ruby/share/modulefiles
+module load wine/1.8.6
 
 module use /users/PZS0645/wiag/local-owens/emc2/share/modulefiles
 module load ctsp    # necessary for writing out WARP3D cut
